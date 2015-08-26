c0fa095ea9a6bb783a265d333904f2a56255fb6d
alan le <alan@vertigo.com>
2015-08-26 16:20:32 -0700

    use images instead of rectangles

--- Win10Exploration/MainPage.xaml.cs
+++ Win10Exploration/MainPage.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -24,6 +15,8 @@ namespace Win10Exploration
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        int itemsToAddCount = 8;
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -39,59 +32,116 @@ namespace Win10Exploration
             ReorderItemsContainer.Items.Clear();
             RepositionItemsContainer.Items.Clear();
         }
+        private void NoTransitionButton_Click(object sender, RoutedEventArgs e)
+        {
+            for (var i = 0; i < itemsToAddCount; i++)
+            {
+                //NoTransitionItemsContainer.Items.Add(new Rectangle() { Fill = new SolidColorBrush(Colors.Red), Margin = new Thickness(10), Height = 100, Width = 100 });
+                NoTransitionItemsContainer.Items.Add(
+                    new Image()
+                    {
+                        Source = new BitmapImage()
+                        {
+                            UriSource = new Uri("ms-appx:///Images/LandscapeImage" + i.ToString() + ".jpg", UriKind.Absolute)
+                        },
+                        Margin = new Thickness(0, 0, 10, 10)
+                    });
+            }
+        }
+
         private void AddDeleteTransitionButton_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 18; i++)
+            for (var i = 0; i < itemsToAddCount; i++)
             {
-                AddDeleteItemsContainer.Items.Add(new Rectangle() { Fill = new SolidColorBrush(Colors.Red), Margin = new Thickness(10), Height = 100, Width = 100 });
+                //AddDeleteItemsContainer.Items.Add(new Rectangle() { Fill = new SolidColorBrush(Colors.Red), Margin = new Thickness(10), Height = 100, Width = 100 });
+                AddDeleteItemsContainer.Items.Add(
+                    new Image()
+                    {
+                        Source = new BitmapImage()
+                        {
+                            UriSource = new Uri("ms-appx:///Images/LandscapeImage" + i.ToString() + ".jpg", UriKind.Absolute)
+                        }, Margin = new Thickness(0,0,10,10)
+                    });
             }
         }
 
         private void ContentTransitionButton_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 18; i++)
+            for (var i = 0; i < itemsToAddCount; i++)
             {
-                ContentItemsContainer.Items.Add(new Rectangle() { Fill = new SolidColorBrush(Colors.Red), Margin = new Thickness(10), Height = 100, Width = 100 });
+                ContentItemsContainer.Items.Add(
+                    new Image()
+                    {
+                        Source = new BitmapImage()
+                        {
+                            UriSource = new Uri("ms-appx:///Images/LandscapeImage" + i.ToString() + ".jpg", UriKind.Absolute)
+                        },
+                        Margin = new Thickness(0, 0, 10, 10)
+                    });
             }
         }
 
         private void EdgeUITransitionButton_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 18; i++)
+            for (var i = 0; i < itemsToAddCount; i++)
             {
-                EdgeUIItemsContainer.Items.Add(new Rectangle() { Fill = new SolidColorBrush(Colors.Red), Margin = new Thickness(10), Height = 100, Width = 100 });
+                EdgeUIItemsContainer.Items.Add(
+                    new Image()
+                    {
+                        Source = new BitmapImage()
+                        {
+                            UriSource = new Uri("ms-appx:///Images/LandscapeImage" + i.ToString() + ".jpg", UriKind.Absolute)
+                        },
+                        Margin = new Thickness(0, 0, 10, 10)
+                    });
             }
         }
 
         private void EntranceTransitionButton_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 18; i++)
+            for (var i = 0; i < itemsToAddCount; i++)
             {
-                EntranceItemsContainer.Items.Add(new Rectangle() { Fill = new SolidColorBrush(Colors.Red), Margin = new Thickness(10), Height = 100, Width = 100 });
+                EntranceItemsContainer.Items.Add(
+                    new Image()
+                    {
+                        Source = new BitmapImage()
+                        {
+                            UriSource = new Uri("ms-appx:///Images/LandscapeImage" + i.ToString() + ".jpg", UriKind.Absolute)
+                        },
+                        Margin = new Thickness(0, 0, 10, 10)
+                    });
             }
         }
 
         private void ReorderTransitionButton_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 18; i++)
+            for (var i = 0; i < itemsToAddCount; i++)
             {
-                ReorderItemsContainer.Items.Add(new Rectangle() { Fill = new SolidColorBrush(Colors.Red), Margin = new Thickness(10), Height = 100, Width = 100 });
+                ReorderItemsContainer.Items.Add(
+                    new Image()
+                    {
+                        Source = new BitmapImage()
+                        {
+                            UriSource = new Uri("ms-appx:///Images/LandscapeImage" + i.ToString() + ".jpg", UriKind.Absolute)
+                        },
+                        Margin = new Thickness(0, 0, 10, 10)
+                    });
             }
         }
 
         private void RepositionTransitionButton_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 18; i++)
+            for (var i = 0; i < itemsToAddCount; i++)
             {
-                RepositionItemsContainer.Items.Add(new Rectangle() { Fill = new SolidColorBrush(Colors.Red), Margin = new Thickness(10), Height = 100, Width = 100 });
-            }
-        }
-
-        private void NoTransitionButton_Click(object sender, RoutedEventArgs e)
-        {
-            for (int i = 0; i < 18; i++)
-            {
-                NoTransitionItemsContainer.Items.Add(new Rectangle() { Fill = new SolidColorBrush(Colors.Red), Margin = new Thickness(10), Height = 100, Width = 100 });
+                RepositionItemsContainer.Items.Add(
+                    new Image()
+                    {
+                        Source = new BitmapImage()
+                        {
+                            UriSource = new Uri("ms-appx:///Images/LandscapeImage" + i.ToString() + ".jpg", UriKind.Absolute)
+                        },
+                        Margin = new Thickness(0, 0, 10, 10)
+                    });
             }
         }
     }
