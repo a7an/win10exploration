ca00cc4a456964280331539353bdea14b7cdb7fe
alan le <alan@vertigo.com>
2015-08-26 16:28:03 -0700

    use null propagration

--- Win10Exploration/MainPage.xaml.cs
+++ Win10Exploration/MainPage.xaml.cs
@@ -1,18 +1,10 @@
 ﻿using System;
-using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Shapes;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Win10Exploration
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         int itemsToAddCount = 8;
@@ -24,20 +16,21 @@ namespace Win10Exploration
 
         private void ResetButton_Click(object sender, RoutedEventArgs e)
         {
-            NoTransitionItemsContainer.Items.Clear();
-            AddDeleteItemsContainer.Items.Clear();
-            ContentItemsContainer.Items.Clear();
-            EdgeUIItemsContainer.Items.Clear();
-            EntranceItemsContainer.Items.Clear();
-            ReorderItemsContainer.Items.Clear();
-            RepositionItemsContainer.Items.Clear();
+            NoTransitionItemsContainer?.Items?.Clear();
+            AddDeleteItemsContainer?.Items?.Clear();
+            ContentItemsContainer?.Items?.Clear();
+            EdgeUIItemsContainer?.Items?.Clear();
+            EntranceItemsContainer?.Items?.Clear();
+            ReorderItemsContainer?.Items?.Clear();
+            RepositionItemsContainer?.Items?.Clear();
         }
         private void NoTransitionButton_Click(object sender, RoutedEventArgs e)
         {
+
             for (var i = 0; i < itemsToAddCount; i++)
             {
-                //NoTransitionItemsContainer.Items.Add(new Rectangle() { Fill = new SolidColorBrush(Colors.Red), Margin = new Thickness(10), Height = 100, Width = 100 });
-                NoTransitionItemsContainer.Items.Add(
+                //NoTransitionItemsContainer?.Items?.Add(new Rectangle() { Fill = new SolidColorBrush(Colors.Red), Margin = new Thickness(10), Height = 100, Width = 100 });
+                NoTransitionItemsContainer?.Items?.Add(
                     new Image()
                     {
                         Source = new BitmapImage()
@@ -53,8 +46,8 @@ namespace Win10Exploration
         {
             for (var i = 0; i < itemsToAddCount; i++)
             {
-                //AddDeleteItemsContainer.Items.Add(new Rectangle() { Fill = new SolidColorBrush(Colors.Red), Margin = new Thickness(10), Height = 100, Width = 100 });
-                AddDeleteItemsContainer.Items.Add(
+                //AddDeleteItemsContainer?.Items?.Add(new Rectangle() { Fill = new SolidColorBrush(Colors.Red), Margin = new Thickness(10), Height = 100, Width = 100 });
+                AddDeleteItemsContainer?.Items?.Add(
                     new Image()
                     {
                         Source = new BitmapImage()
@@ -69,7 +62,7 @@ namespace Win10Exploration
         {
             for (var i = 0; i < itemsToAddCount; i++)
             {
-                ContentItemsContainer.Items.Add(
+                ContentItemsContainer?.Items?.Add(
                     new Image()
                     {
                         Source = new BitmapImage()
@@ -85,7 +78,7 @@ namespace Win10Exploration
         {
             for (var i = 0; i < itemsToAddCount; i++)
             {
-                EdgeUIItemsContainer.Items.Add(
+                EdgeUIItemsContainer?.Items?.Add(
                     new Image()
                     {
                         Source = new BitmapImage()
@@ -101,7 +94,7 @@ namespace Win10Exploration
         {
             for (var i = 0; i < itemsToAddCount; i++)
             {
-                EntranceItemsContainer.Items.Add(
+                EntranceItemsContainer?.Items?.Add(
                     new Image()
                     {
                         Source = new BitmapImage()
@@ -117,7 +110,7 @@ namespace Win10Exploration
         {
             for (var i = 0; i < itemsToAddCount; i++)
             {
-                ReorderItemsContainer.Items.Add(
+                ReorderItemsContainer?.Items?.Add(
                     new Image()
                     {
                         Source = new BitmapImage()
@@ -133,7 +126,7 @@ namespace Win10Exploration
         {
             for (var i = 0; i < itemsToAddCount; i++)
             {
-                RepositionItemsContainer.Items.Add(
+                RepositionItemsContainer?.Items?.Add(
                     new Image()
                     {
                         Source = new BitmapImage()
